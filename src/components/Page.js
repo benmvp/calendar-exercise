95a9f567d94e6818134fbcea303e49b06a073e63
Ben Ilegbodu <ben@benmvp.com>
2017-04-07 13:28:35 -0700

    Add initial app structure

---
+++ src/components/Page.js
@@ -0,0 +1,16 @@
+import React, {PureComponent} from 'react';
+
+import './Page.css';
+
+export default class Page extends PureComponent {
+    render() {
+        return (
+            <div className="page">
+                <header>
+                    <h1 className="page__title">Daily Agenda</h1>
+                </header>
+
+            </div>
+        );
+    }
+}
