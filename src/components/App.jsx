09dd5de8e46a1c2edcbe676f3d258886f59d5942
Julia Wong <juliawong05@gmail.com>
2018-04-07 15:06:18 -0700

    converted components to jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React, {PureComponent} from 'react';
 import Page from './Page';
 
-export default class App extends PureComponent {
+export default class App extends React.PureComponent {
     render() {
+      return(
         return (<Page />);
+      );
     }
 };
