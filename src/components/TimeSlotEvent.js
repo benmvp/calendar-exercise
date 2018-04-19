4a791681ee0fd94b5966c4a1be5fa8af00c3f588
sallyyeng <sallyyeng@gmail.com>
2018-04-18 18:50:19 -0700

    [JS/CSS] Display past events faded out

--- src/components/TimeSlotEvent.js
+++ src/components/TimeSlotEvent.js
@@ -11,15 +11,17 @@ export default class TimeSlotEvent extends PureComponent {
 
     render() {
         let {
-            event: {title, color},
+            event: {title, color, start},
             onSelect,
         } = this.props;
 
         // TODO: Need a way to determine that the event is in the past so that it
         // can be displayed faded-out
+        let isFaded = () => new Date(start) < new Date() ? 0.5 : false;
+        const style = {opacity: isFaded()};
 
         return (
-            <button className={`time-slot-event time-slot-event--${color}`} onClick={onSelect}>
+            <button className={`time-slot-event time-slot-event--${color}`} style={style} onClick={onSelect}>
                 {title}
             </button>
         );
