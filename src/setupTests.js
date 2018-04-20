d6407aa14e3ee8014dd764fa7a7e64862a74d40b
sallyyeng <sallyyeng@gmail.com>
2018-04-20 14:25:15 -0700

    Make testing functions avail in all test files without import

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,6 +7,12 @@ import Adapter from 'enzyme-adapter-react-16';
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 
+// Make functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.spy = spy;
+
 // Fail tests on any warning
 console.error = message => {
    throw new Error(message);
