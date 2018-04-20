300615f153967e7cf7a555747ca5aaea25d1c320
sallyyeng <sallyyeng@gmail.com>
2018-04-20 00:15:17 -0700

    Replace react-test-renderer with enzyme shallow testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+describe('App.js', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+
+        ReactDOM.render(<App />, div);
+    });
+
+    it('renders correctly', () => {
+        const component = shallow(<App />);
+
+        expect(component).toMatchSnapshot();
+    });
+
 });
 
-it('renders correctly', () => {
-  const component = renderer.create(<App />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-})
